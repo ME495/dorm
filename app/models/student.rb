5f889920440d6c4a7612f62cccd84be789addd90
ME495 <ME495@qq.com>
2019-12-03 13:27:20 +0800

    修改Student的room_id外键约束，使其可以为空.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,14 @@
+class AssociationsValidator < ActiveModel::Validator
+  def validate(student)
+    if not student.room_id.nil? and not Room.exists?(student.room_id)
+      student.errors[:room_id] << "must exist."
+    end
+  end
+end
+
 class Student < ApplicationRecord
-  belongs_to :room
+  belongs_to :room, optional: true
+  validates_with AssociationsValidator
   has_many :lectures, dependent: :destroy
   validates :student_number, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
