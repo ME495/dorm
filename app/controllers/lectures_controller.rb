29ccfbc67eea8f3fa1f7b67f618647d0a365d0d8
WANG\Wyf <360022263@qq.com>
2019-12-18 20:13:57 +0800

    “完成添加小讲堂功能

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,14 +1,22 @@
 class LecturesController < ApplicationController
-  def new
+ 	def new
 		@lecture = Lecture.new
 	end
 	def create
-		render plain: params[:lecture].inspect
-		#@lecture = Lecture.new(lecture_params)
-		#if @lecture.save
-	#		redirect_to @lecture
-	#	else
-	#		render 'new'
-	#	end
+		# render plain: params[:lecture].inspect
+		
+		@lecture = Lecture.new(lecture_params)
+		@lecture.student_id = session[:id]
+		@student = Student.find(session[:id])
+		@room = Room.find(@student.room_id)
+		@lecture.suite = @room.suite
+		puts @lecture.suite
+		if @lecture.save!
+			render 'show'
+		end
 	end
+	private
+	def lecture_params
+		params.require(:lecture).permit(:title,:start_time,:end_time,:category,:student_id,:content,:suite)
+	end	
 end
