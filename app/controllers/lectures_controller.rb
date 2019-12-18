124a99f2430b80bf46c12a272f0ad291c66ac6ef
YeefyisOK <53204786+YeefyisOK@users.noreply.github.com>
2019-12-18 21:04:41 +0800

    Update lectures_controller.rb

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,4 +1,12 @@
 class LecturesController < ApplicationController
+  def index
+    @lectures = Lecture.all
+  end
+
+  def show
+    @lecture = Lecture.find(params[:id])
+    @student = Student.find(@lecture.student_id)
+  end
  	def new
 		@lecture = Lecture.new
 	end
